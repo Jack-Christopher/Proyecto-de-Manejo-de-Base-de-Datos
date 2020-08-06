fc7f519ede87a762d84728159882594df450e43c
Jack-Christopher <jhuaihuah@unsa.edu.pe>
2020-08-06 09:18:37 -0500

    Aaptacíón del 'main' para el Facade

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include "login.h"
+#include "facade.h"
 
-#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    LogIn w;
-    w.show();
-    return a.exec();
+    Facade f;
+
+    f.ejecutarConfiguracion();
+
+    auto smartPointerToApp = f.getSmartPointer(argc, argv);
+
+    auto resultado = f.ejecutarAplicacion(smartPointerToApp);
+
+    return resultado;
 }
